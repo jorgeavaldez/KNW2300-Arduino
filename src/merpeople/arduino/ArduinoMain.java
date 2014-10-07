7de5bb212ee3437ebbba81ae39c6e217c8933cf7
Jorge Valdez <valdez.a.jorge@gmail.com>
2014-10-06 19:07:48 -0500

    Added constants and body.
    Assigned static constants to descriptive names and added the basic bare
    boilerplate bones of the Main method.

--- src/merpeople/arduino/ArduinoMain.java
+++ src/merpeople/arduino/ArduinoMain.java
@@ -4,9 +4,32 @@ import rxtxrobot.*;
 
 public class ArduinoMain 
 {
-
+	final static int BUMP_SENSOR = 1; //A1
+	final static int INFRARED_SENSOR = 0; //A0
+	final static int TURBIDITY_SENSOR = 4; //A4
+	final static int SALINITY_SENSOR = 5; //A5
+	
+	final static int ARM_SERVO = 0; //D7
+	final static int TRAPDOOR_SERVO = 0; //D8
+	
+	final static int LEFT_MOTOR = RXTXRobot.MOTOR1; 
+	final static int RIGHT_MOTOR = RXTXRobot.MOTOR2; 
+	
+	//D2 - Left Motor Encoder
+	//D3 - Right Motor Encoder
+	//D5 - Left Motor Controller
+	//D6 - Right Motor Controller
+	
+	static RXTXRobot r = new ArduinoNano();
+		
 	public static void main(String[] args) 
 	{
-
+		r.setPort("COM5");
+		r.setVerbose(true);
+		r.connect();
+		
+		//BODY SHIT
+		
+		r.close();
 	}
 }
