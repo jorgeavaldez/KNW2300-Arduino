fe6e1dc83cb54e1740108a41ae8a2a67623674bd
Jorge Valdez <valdez.a.jorge@gmail.com>
2014-10-07 23:53:41 -0500

    Fixed the stuff for testing.

--- src/merpeople/arduino/ArduinoMain.java
+++ src/merpeople/arduino/ArduinoMain.java
@@ -9,8 +9,8 @@ public class ArduinoMain
 	final static int TURBIDITY_SENSOR = 4; //A4
 	final static int SALINITY_SENSOR = 5; //A5
 	
-	final static int ARM_SERVO = 0; //D7
-	final static int TRAPDOOR_SERVO = 0; //D8
+	final static int ARM_SERVO = 7; //D7
+	final static int TRAPDOOR_SERVO = 8; //D8
 	
 	final static int LEFT_MOTOR = RXTXRobot.MOTOR1; 
 	final static int RIGHT_MOTOR = RXTXRobot.MOTOR2; 
@@ -28,7 +28,28 @@ public class ArduinoMain
 		r.setVerbose(true);
 		r.connect();
 		
-		//BODY SHIT
+		//Bullet 1
+		//Move forward for 90 Ticks
+		//Using it to measure how much 1 rotation gives us, and then determining how many ticks are 3 meters
+		r.runEncodedMotor(RXTXRobot.MOTOR1, 255, 90, RXTXRobot.MOTOR2, 255, 90);
+		
+		r.attachServo(RXTXRobot.SERVO1, ARM_SERVO); //Connect the servos to the Arduino 
+		r.moveServo(RXTXRobot.SERVO1, 0);
+		
+		/*
+		//Bullet 2
+		//Run for 500 ticks
+		r.runEncodedMotor(RXTXRobot.MOTOR1, -255, 500, RXTXRobot.MOTOR2, 255, 500);
+		
+		//Bullet 3
+		//
+		r.attachServo(RXTXRobot.SERVO1, ARM_SERVO); //Connect the servos to the Arduino 
+		
+		r.moveServo(RXTXRobot.SERVO1, 0); // Move
+		Servo 1 to location 0
+		r.moveServo(RXTXRobot.SERVO1, 90); // Move Servo 1 to location 90
+		r.moveServo(RXTXRobot.SERVO1, 180); // Move Servo 2 to location 180
+		*/
 		
 		r.close();
 	}
